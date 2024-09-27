88641fc5142930e3f8129cfbc2f5cf8ef1dbd118
VitalyTance <vitalytanceforwork@gmail.com>
2024-09-28 02:46:03 +1000

    Third commit

---
+++ MyIndependent9/car_reg/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Car, Detail, RegDetail
+# Register your models here.
+
+
+class RegDetailInLine(admin.StackedInline):
+    model = RegDetail
+    extra = 1
+
+
+class DetailAdmin(admin.ModelAdmin):
+    inlines = [RegDetailInLine]
+
+
+admin.site.register(Detail, DetailAdmin)
+
+
+class DetailInLine(admin.StackedInline):
+    model = Detail
+    extra = 1
+    show_change_link = True
+
+
+class CarAdmin(admin.ModelAdmin):
+    inlines = [DetailInLine]
+
+
+admin.site.register(Car, CarAdmin)
